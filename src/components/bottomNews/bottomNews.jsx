464ea4e8cec471cb7ae2d53ecf4fd5f4e368ec37
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-01-25 22:22:18 +0530

    image updated

--- src/components/bottomNews/bottomNews.jsx
+++ src/components/bottomNews/bottomNews.jsx
@@ -5,11 +5,20 @@ import bottomImage2 from "../../assets/image-top-laptops.jpg";
 import bottomImage3 from "../../assets/image-gaming-growth.jpg";
 
 function BottomNews({ bottomData }) {
+  const imgPath = (id) => {
+    if (id === "01") {
+      return bottomImage1;
+    } else if (id === "02") {
+      return bottomImage2;
+    } else {
+      return bottomImage3;
+    }
+  };
   const bottomNews = bottomData.map((data) => {
     return (
       <div key={data.id} className="bottom-container">
         <div className="image-container">
-          <img src={data.img} alt="news container" />
+          <img src={imgPath(data.id)} alt="news container" />
         </div>
         <div className="bottom-news">
           <h1>{data.id}</h1>
